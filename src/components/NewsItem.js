5f36960270b918c200cee42cfa827d0f74fc08e1
vishd91 <vishd91@gmail.com>
2024-08-11 20:34:17 +0530

    Update NewsItem.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let {title, description, imageUrl} = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
